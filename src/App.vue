55f9df654263afca74db6fca43465bd8cee47434
abemaru <ch4nge.lightening@gmail.com>
2022-11-03 09:34:48 +0900

    何も進んでないけど

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
 import TheWelcome from "./components/TheWelcome.vue";
+import Plot from "./components/PlotSampleData.vue";
 </script>
 
 <template>
@@ -19,6 +20,7 @@ import TheWelcome from "./components/TheWelcome.vue";
   </header>
 
   <main>
+		<Plot />
     <TheWelcome />
   </main>
 </template>
